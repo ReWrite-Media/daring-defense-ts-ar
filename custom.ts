5ffdc0a423ef9a70effda026c59f8bea0b60b5f5
Sam Stuart <sam@tcdevworld.com>
2021-09-14 03:07:11 +0100

    Minor changes to custom dropItemsIndividually block

--- custom.ts
+++ custom.ts
@@ -1,16 +1,15 @@
 /**
  * Custom blocks
  */
- namespace agent {
+namespace agent {
     /**
      * Have the agent drop fish sequentially and not in bulk
      * @param direction the direction the will to drop the fish
      * @param slot the slot in the agent's inventory to drop from
      * @param quantity the number of items to drop
      */
-    //% block="agent drop items one at a time $direction from slot $slot quantity $quantity" blockId=agentdropfish
-    //% groups=['Actions']
-    export function dropItems(direction: SixDirection, slot: number, quantity: number) {
+    //% block="agent drop items one at a time $direction from slot $slot quantity $quantity" blockId=agentdropitemsindividually
+    export function dropItemsIndividually(direction: SixDirection, slot: number, quantity: number) {
         let count = agent.getItemCount(slot);
         if(quantity < count) {
             count = quantity;
